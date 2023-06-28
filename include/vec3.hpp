60a63de1ccdfa438a9797b957147607d54cb822c
Marko Ilioski <markoilioski21@gmail.com>
2023-06-28 23:08:07 +0200

    Drew a sphere

--- include/vec3.hpp
+++ include/vec3.hpp
@@ -68,6 +68,7 @@ inline vec3 operator/(vec3 v, double t) {
     return (1/t) * v;
 }
 
+// dot is the dot product DON'T GET CONFUSED
 inline double dot(const vec3 &u, const vec3 &v) {
     return u.e[0] * v.e[0]
            + u.e[1] * v.e[1]
