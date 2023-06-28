620a5abed96237c2f6e74c456c3b8952a1521143
Marko Ilioski <markoilioski21@gmail.com>
2023-06-29 00:02:50 +0200

    Copied the list class.

---
+++ include/hittable_list.hpp
@@ -0,0 +1,32 @@
+//
+// Created by 38975 on 28.6.2023.
+//
+
+#ifndef HOMEWORK_HELL_HITTABLE_LIST_HPP
+#define HOMEWORK_HELL_HITTABLE_LIST_HPP
+
+
+#include "hittable.hpp"
+
+#include <memory>
+#include <vector>
+
+using std::shared_ptr;
+using std::make_shared;
+
+class hittable_list : public hittable {
+public:
+    std::vector<shared_ptr<hittable>> objects;
+
+    hittable_list();
+    hittable_list(shared_ptr<hittable> object);
+
+    void clear();
+    void add(shared_ptr<hittable> object);
+
+    virtual bool hit(
+            const ray& r, double t_min, double t_max, hit_record& rec) const override;
+
+};
+
+#endif //HOMEWORK_HELL_HITTABLE_LIST_HPP
