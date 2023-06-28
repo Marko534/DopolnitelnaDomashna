45b18460eb9f8e4a37ba59b39c84d5790a28108c
Marko Ilioski <markoilioski21@gmail.com>
2023-06-28 22:31:08 +0200

    Made it use a ray and have a different background

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
 #include "../include/vec3.hpp"
 #include "../include/color.hpp"
+#include "../include/ray.hpp"
 
 #include <iostream>
 #include <fstream>
 #include <string>
+
 const std::string FILE_OUTPUT = "../output.ppm";
+
+color ray_color(const ray& r){
+    vec3 unit_direction = unit_vector(r.direction());
+    double t = 0.5*(unit_direction.y()+1);
+    return (1.0f-t)*color (1.0f, 1.0f, 1.0f) + t*color (0.5f,0.7f, 1.0f);
+}
 
 int main() {
     //Makeing a file
-
     std::ofstream file(FILE_OUTPUT);
-
     if (!file) {
         std::cerr << "Error opening file for writing." << std::endl;
         return 1;
@@ -18,8 +24,20 @@ int main() {
 
     // Image
 
-    const int image_width = 256;
-    const int image_height = 256;
+    const double aspect_ratio = 16.0f / 9.0f;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+
+    double viewport_height  = 2.0f;
+    double viewport_width = aspect_ratio * viewport_height;
+    double focal_length = 1.0;
+
+    vec3 origin = point3 (0,0,0);
+    vec3 horizontal = vec3(viewport_width, 0, 0);
+    vec3 vertical = vec3(0 , viewport_height,0);
+    vec3 lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0,0,focal_length);
 
     // Render
 
@@ -28,10 +46,15 @@ int main() {
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1),0.25f);
+            double u = double(i) / (image_width-1);
+            double v = double (j) / (image_height-1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            color pixel_color = ray_color(r);
             write_color(file, pixel_color);
         }
     }
     std::cerr << "\nDone.\n";
+
+
 
 }
