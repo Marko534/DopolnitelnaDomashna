13ae747db8260a614a913d5ae0cfb1ee588538d7
Marko Ilioski <markoilioski21@gmail.com>
2023-06-28 09:03:35 +0200

    First picture.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <fstream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    //Makeing a file
+
+    std::ofstream file("output.ppm");
+    if (!file) {
+        std::cerr << "Error opening file for writing." << std::endl;
+        return 1;
+    }
+
+    // Image
+
+    const int image_width = 256;
+    const int image_height = 256;
+
+    // Render
+
+    file << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height-1; j >= 0; --j) {
+        for (int i = 0; i < image_width; ++i) {
+//            std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+            auto r = double(i) / (image_width-1);
+            auto g = double(j) / (image_height-1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            file << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+    std::cerr << "\nDone.\n";
 }
