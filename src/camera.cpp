28a65600de2458bfbcb7c0530c3e0614eee8e543
Marko Ilioski <markoilioski21@gmail.com>
2023-06-30 00:14:09 +0200

    Camera has fov now.

--- src/camera.cpp
+++ src/camera.cpp
@@ -10,6 +10,14 @@ camera::camera(double vfov, double aspect_ratio) {
     double  h = tan(theta/2);
     double viewport_height = 2.0*h;
     double viewport_width  = aspect_ratio * viewport_height;
+
+    double focal_length = 1.0;
+
+    origin = point3(0, 0, 0);
+    horizontal = vec3(viewport_width, 0.0, 0.0);
+    vertical = vec3(0.0, viewport_height, 0.0);
+    lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+
 }
 
 
