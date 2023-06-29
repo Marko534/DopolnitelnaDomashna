ddb0cdf89a3e96e98bf409020e37ab438745b997
Marko Ilioski <markoilioski21@gmail.com>
2023-06-29 18:32:49 +0200

    Made randoms the old fashioned way

--- include/rtweekend.hpp
+++ include/rtweekend.hpp
@@ -8,7 +8,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
-
+#include <cstdlib>
 
 // Usings
 
@@ -28,6 +28,15 @@ inline double degrees_to_radians(double degrees) {
 }
 
 // Common Headers
+
+//Random numb [0,1)
+inline double random_double(){
+    return rand()/(RAND_MAX+1.0f);
+}
+
+inline double random_double(double min, double max){
+    return min + (max-min)*random_double();
+}
 
 #include "ray.hpp"
 #include "vec3.hpp"
